101e49b8df6f54f765bdfffde655b0abcfd3eda7
k0bek <kubakiebala@gmail.com>
2024-08-05 13:53:27 +0200

    feat: create inital frontend project setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@nextui-org/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Frontend initialized</p>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-secondary-500">
+      <Button color="primary">Button</Button>
     </main>
   );
 }
