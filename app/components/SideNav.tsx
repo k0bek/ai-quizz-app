c722127c5a81d17798047c628d6a49f80b9ae939
faustyna77 <faustinmisiura@gmail.com>
2024-08-07 16:57:33 +0200

    corection2

--- app/components/SideNav.tsx
+++ app/components/SideNav.tsx
@@ -1,27 +1,3 @@
-
-'use client'
-/*
-import Link from 'next/link';
-import React from 'react';
-
-const SideNav = () => {
-  return (
-    <div className='w-64 h-full  bg-white p-4'>
-      <ul className='space-y-2'>
-        <li>
-          <Link href="/" className='block py-2 px-4 rounded hover:bg-gray-100 transition-colors'>
-            Dashboard
-          </Link>
-        </li>
-       
-      </ul>
-    </div>
-  );
-};
-
-export default SideNav;
-*/
-
 'use client';
 
 import Link from 'next/link';
@@ -36,7 +12,7 @@ const SideNav = () => {
             Dashboard
           </Link>
         </li>
-        {/* Add more links here */}
+        {}
       </ul>
     </div>
   );
