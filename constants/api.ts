580db6d1f06fc1cf561af034368431e58f709308
faustyna77 <faustinmisiura@gmail.com>
2024-09-12 13:03:40 +0200

    fix

--- constants/api.ts
+++ constants/api.ts
@@ -7,21 +7,16 @@ export const refreshTokenUrl = `${API_BASE_URL}/api/auth/refresh-token`;
 export const currentProfileUrl = `${API_BASE_URL}/api/profile`;
 export const generateQuizUrl = `${API_BASE_URL}/api/quiz/generate-quiz`;
 export const createQuizUrl = `${API_BASE_URL}/api/quiz/create-quiz`;
-
 export const deleteQuiz = `${API_BASE_URL}/api/quiz/`;
 export const quizListUrl = `${API_BASE_URL}/api/quiz`;
 export const updateQuizStatusUrl = `${API_BASE_URL}/api/quiz/`;
-
 export const deleteQuizUrl = `${API_BASE_URL}/api/quiz/`;
-
-
-
-
-
-
 export const singleQuizUrl = `${API_BASE_URL}/api/quiz/`;
 export const updateQuizQuestionsUrl = `${API_BASE_URL}/api/questionsandanswers/update-question`;
 export const updateAvailabilityUrl = `${API_BASE_URL}/api/quiz/`;
 export const updateQuizDataUrl = `${API_BASE_URL}/api/quiz/`;
 export const createQuestionUrl = `${API_BASE_URL}/api/questionsandanswers/create-question`;
 export const deleteQuestionUrl = `${API_BASE_URL}/api/questionsandanswers/`;
+
+
+
