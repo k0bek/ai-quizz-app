ec30eea1d54a3ea3627d35ee68847b9c6da98fe2
faustyna77 <faustinmisiura@gmail.com>
2024-09-24 15:07:29 +0200

    delete backbutton

--- app/[locale]/(generate-create-quiz)/layout.tsx
+++ app/[locale]/(generate-create-quiz)/layout.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/shared/Header";
 import React, { ReactNode } from "react";
 import Stepper from "./(generate-quiz)/components/StatusIndicator/Stepper";
 import StepperProvider from "@/app/context/StepContext";
-import BackButton from "./(generate-quiz)/components/buttons/BackButton";
+
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
@@ -10,7 +10,7 @@ const layout = ({ children }: { children: ReactNode }) => {
       <main>
         <Header />
         <div className="relative">
-          <BackButton />
+         
         </div>
         <section className="md:w-[55rem] mx-auto gap-6 flex flex-col">
           <Stepper />
